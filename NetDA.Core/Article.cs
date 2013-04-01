4c1b2d95b5159fa1bcfed6732b9dc737446b6f2f
Scott Blomquist <github@scott.blomqui.st>
2013-04-01 09:51:50 -0700

    Actual data model code.

--- NetDA.Core/Article.cs
+++ NetDA.Core/Article.cs
@@ -4,8 +4,13 @@ namespace NetDA.Core
 {
 	public class Article
 	{
-		public Article ()
+		public string Title { get; private set; }
+		public string Url { get; private set; }
+
+		public Article (string title, string url)
 		{
+			Title = title;
+			Url = url;
 		}
 	}
 }
--- NetDA.Core/Monologue.cs
+++ NetDA.Core/Monologue.cs
@@ -1,12 +1,53 @@
 using System;
+using System.IO;
+using System.Net;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Xml;
 
 namespace NetDA.Core
 {
 	public class Monologue
 	{
+		public delegate void ArticlesLoadedHandler();
+		public event ArticlesLoadedHandler ArticlesLoaded;
+		public ObservableCollection<Article> Articles {get;set;}
+
 		public Monologue ()
 		{
+			Articles = new ObservableCollection<Article> ();
 		}
+
+		public void Initialize() {
+			var wc = new WebClient();
+			wc.OpenReadCompleted += HandleOpenReadCompleted;
+			wc.OpenReadAsync (new Uri("http://go-mono.com/monologue/index.rss"));
+		}
+
+		public void HandleOpenReadCompleted (object sender, OpenReadCompletedEventArgs e)
+		{
+			string title;
+			string link;
+
+			using (var result = e.Result)
+			using (var xr = XmlReader.Create(result)) {
+				while (xr.Read()) {
+					if (xr.IsStartElement ()) {
+						if (xr.Name == "item") {
+							xr.ReadToDescendant ("title");
+							title = xr.ReadString ();
+							xr.ReadToNextSibling ("link");
+							link = xr.ReadString ();
+							Articles.Add (new Article (title, link));
+						}
+					}
+				}
+			}
+			if (ArticlesLoaded != null) {
+				ArticlesLoaded();
+			}
+		}
+
+
 	}
 }
-
